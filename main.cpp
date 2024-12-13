0e4a480af4de2f1c50c6bfc7c3b3e890bbeeaa7d
byn17Z <147910899+byn17Z@users.noreply.github.com>
2024-12-13 22:43:38 +0800

    update
    cmd: 
    Compiler <src_path> <target_path>

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
     // you need absolute path to use gettoken
     /*
     auto token=gettoken("/Users/zhaokunxiang/clion/compiler/Test/source1.cpp");
@@ -11,12 +11,26 @@ int main(){
     auto sym=analysis(tree);
     tree.getNode();
     */
-    string file;
-    string path;
-    cout << "src file: " << endl;
-    getline(cin, file);
-    cout << "target path: " << endl;
-    getline(cin, path);
-    compile(file, path);
+    if (argc == 3) {
+        compile(argv[1], argv[2]);
+    }
+    // string input;
+    // string file = "";
+    // string path = "";
+    // getline(cin, input);
+    // if (input.length() == 0) {return 0;}
+    // else {
+    //     for (int i = 0; i < input.length(); i++) {
+    //         if (input[i] == '|') {
+    //             if (i >= input.length()-1) {break;}
+    //             for (int j = i+1; j < input.length(); j++) {
+    //                 path += input[j];
+    //             }
+    //             break;
+    //         }
+    //         file += input[i];
+    //     }
+    // }
+    // compile(file, path);
     return 0;
         }
